3d8433099bdda0198106380b36b3dfee9d30839a
areumz <ksah1016@naver.com>
2022-07-23 21:16:30 +0900

    Fix: SNS 클릭시 url 에러 수정

--- src/components/Main/Aboutme/Aboutme.jsx
+++ src/components/Main/Aboutme/Aboutme.jsx
@@ -1,55 +1,71 @@
 import React from "react";
-import styles from './Aboutme.module.css';
-import facebook from '../../../assets/Facebook.svg';
-import twitter from '../../../assets/Twitter.svg';
-import instagram from '../../../assets/Instagram.svg';
-import github from '../../../assets/Github.svg';
-import profile from '../../../assets/profile.jpg';
+import styles from "./Aboutme.module.css";
+import facebook from "../../../assets/Facebook.svg";
+import twitter from "../../../assets/Twitter.svg";
+import instagram from "../../../assets/Instagram.svg";
+import github from "../../../assets/Github.svg";
+import profile from "../../../assets/profile.jpg";
 
 const Aboutme = () => {
-    return (
-        <aside className={styles.about}>
-            <h2>About Me</h2>
-                <img src={profile} alt="프로필 이미지" className={styles.userProfile} />
-                <p className={styles.userName}>Chilli</p>
-                <p className={styles.userDescription}>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+  return (
+    <aside className={styles.about}>
+      <h2>About Me</h2>
+      <img src={profile} alt="프로필 이미지" className={styles.userProfile} />
+      <p className={styles.userName}>Chilli</p>
+      <p className={styles.userDescription}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      </p>
 
-                <h3>Categories</h3>
-                <ul className={styles.categories}>
-                    <li><button href="#">Life</button></li>
-                    <li><button href="#">Style</button></li>
-                    <li><button href="#">Tech</button></li>
-                    <li><button href="#">Sport</button></li>
-                    <li><button href="#">Photo</button></li>
-                    <li><button href="#">Develop</button></li>
-                    <li><button href="#">Music</button></li>
-                </ul>
+      <h3>Categories</h3>
+      <ul className={styles.categories}>
+        <li>
+          <button href="#">Life</button>
+        </li>
+        <li>
+          <button href="#">Style</button>
+        </li>
+        <li>
+          <button href="#">Tech</button>
+        </li>
+        <li>
+          <button href="#">Sport</button>
+        </li>
+        <li>
+          <button href="#">Photo</button>
+        </li>
+        <li>
+          <button href="#">Develop</button>
+        </li>
+        <li>
+          <button href="#">Music</button>
+        </li>
+      </ul>
 
-                <h3>Follow</h3>
-                <ul className={styles.sns}>
-                    <li>
-                        <button onClick={()=> window.open('www.facebook.com', '_blank')}>
-                            <img src={facebook} alt="Facebook" />
-                        </button>
-                    </li>
-                    <li>
-                        <button onClick={()=> window.open('www.twitter.com', '_blank')}>
-                            <img src={twitter} alt="Twitter" />
-                        </button>
-                    </li>
-                    <li>
-                        <button onClick={()=> window.open('www.instagram.com', '_blank')}>
-                            <img src={instagram} alt="Instagram" />
-                        </button>
-                    </li>
-                    <li>
-                        <button onClick={()=> window.open('www.github.com', '_blank')}>
-                            <img src={github} alt="GitHub" />
-                        </button>
-                    </li>
-                </ul>
+      <h3>Follow</h3>
+      <ul className={styles.sns}>
+        <li>
+          <a href="www.facebook.com" target="_blank">
+            <img src={facebook} alt="Facebook" />
+          </a>
+        </li>
+        <li>
+          <a href="www.twitter.com" target="_blank">
+            <img src={twitter} alt="Twitter" />
+          </a>
+        </li>
+        <li>
+          <a href="www.instagram.com" target="_blank">
+            <img src={instagram} alt="Instagram" />
+          </a>
+        </li>
+        <li>
+          <a href="www.github.com" target="_blank">
+            <img src={github} alt="GitHub" />
+          </a>
+        </li>
+      </ul>
     </aside>
-    )
-}
+  );
+};
 
 export default Aboutme;
